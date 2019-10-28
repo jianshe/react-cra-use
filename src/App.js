1261627ae76a37918d8fb0e8d8115131c0df643e
jianshe <zhangshi789wei@163.com>
2019-10-28 23:34:02 +0800

    add react lifeCycle and mindmaster

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import LifeCycle from "./pages/LifeCycle";
-import ClassComponent from "./pages/ClassComponent"
+import LifeCycle from "./pages/LifeCycle";
+// import ClassComponent from "./pages/ClassComponent"
 // import FunComponent from "./pages/FunComponent"
 // import EventHandle from "./pages/EventHandle"
 class App extends Component {
   render() {
-    function tellme(msg) {
-      console.log('子调用父函数时传递的值:' + msg)
-    }
+    // function tellme(msg) {
+    //   console.log('子调用父函数时传递的值:' + msg)
+    // }
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        {/* <LifeCycle /> */}
-        <ClassComponent/>
+        <LifeCycle />
+        {/* <ClassComponent/> */}
         {/* <FunComponent/> */}
         {/* <EventHandle name="我是input输入框要传递的值" tellme={tellme}/> */}
       </div>
--- src/pages/LifeCycle.js
+++ src/pages/LifeCycle.js
@@ -4,12 +4,67 @@ class LifeCycle extends Component {
     super(...arguments);
     this.state = {
       counter: 0
-    }
+    };
+    console.log('constructor', this.state);
   }
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps', props, state)
+    // 返回null不对state做任何的改变，返回一个对象代表对之前的属性做一定的改变
+    return state.counter !== 5 ? null : { counter: 0 }
+    // return null;
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('getSnapshotBeforeUpdate',prevProps,prevState)
+    return null;
+  }
+  // UNSAFE_componentWillMount() {
+  //   console.log('componentWillMount', this.state);
+  // }
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log('shouldComponentUpdate', this.state)
+    console.log('shouldComponentUpdate', nextProps, nextState);
+    return nextState.counter !== 3;
+  }
+  // UNSAFE_componentWillUpdate() {
+  //   console.log('componentWillUpdate', this.state);
+  // }
+  componentDidUpdate() {
+    console.log('componentDidUpdate', this.state);
+  }
+  setCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
   render() {
+    console.log('render', this.state);
+    const { counter } = this.state;
     return (
       <div>
-        { this.state.counter}
+        <h1>LifeCycle</h1>
+        <p onClick={this.setCounter}>{counter}</p>
+        {counter !== 2 && <Foo counter={counter} />}
+      </div>
+    );
+  }
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+}
+
+class Foo extends Component {
+  // UNSAFE_componentWillReceiveProps(props, state) {
+  //   console.log('componentWillReceiveProps', props, state);
+  // }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  render() {
+    const { counter } = this.props;
+    return (
+      <div>
+        <h1>Foo</h1>
+        <p>{counter}</p>
       </div>
     );
   }
